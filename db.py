5eb0f815b25308a7634527fb9de059327d20508c
n7tms@yahoo.com <n7tms@yahoo.com>
2023-01-26 17:05:44 -0700

    clean

--- db.py
+++ db.py
@@ -4,31 +4,4 @@ class DB:
     def __init__(self):
         self.dbname = 'test.db'
 
-DB_NAME = 'mm_test2.db'
-checkpoints = {}
 
-def get_checkpoints() -> list:
-    """return a list of checkpoints"""
-    cn,cur = None,None
-    cn = sqlite3.connect(DB_NAME)
-    cur = cn.cursor()
-
-    if len(checkpoints) > 0:
-        checkpoints.clear()
-
-
-    res = cur.execute("select CheckpointID,CPName from Checkpoints")
-    for x in res:
-        checkpoints[x[1]] = x[0]
-    
-    print(list(checkpoints.values()))
-
-    if 'M1' in checkpoints:
-        print('is')
-
-    print(list(checkpoints.keys())[0])
-    return list(checkpoints.keys())
-
-
-
-print(get_checkpoints())
